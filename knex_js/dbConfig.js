b2c891527c6a4bffdbe7ce93fa01e96a4e770895
Abhinav Anand <abhinavanand072@gmail.com>
2025-10-09 16:53:36 +0530

    minor bug fix in dbConfig file

--- knex_js/dbConfig.js
+++ knex_js/dbConfig.js
@@ -2,10 +2,10 @@ import knex from "knex";
 import config from "./knexfile.js";
 
 // Determine which configuration to load
-const enviroment = process.env.NODE_ENV === 'production' ? 'production' : 'developement';
+const enviroment = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 // Select the 'development' configuration from knexfile.js
-const db = knex(config.development);
+const db = knex(config[enviroment]);
 
 // Export the single, reusable connection instance
 export default db;
