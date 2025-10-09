f2092840d52aec4de0761fd5150a07e03697baea
Abhinav Anand <abhinavanand072@gmail.com>
2025-10-09 16:35:23 +0530

    Applied HttpOnly Cookies for JWT

--- authenticateUser_jwt/authenticateUser_jwt.js
+++ authenticateUser_jwt/authenticateUser_jwt.js
@@ -2,18 +2,21 @@ import jwt from 'jsonwebtoken';
 
 // A authentication middleware that user jwt auth
 export const authenticateUser = async (req, res, next) => {
-    // get the authorization header
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+    // get token from HttpOnly cookie
+    const token = req.cookies.authToken; 
 
     // if there's no token return an unautherized error
     if (!token) {
-        return res.status(401).json({message: 'Autherization Token is missing.'})
+        // Clear the token if one was partially set or expired in the client
+        res.clearCookie('authToken');
+        return res.status(401).json({message: 'Autherization Token is missing. Please log in.'});
     }
 
     // verify the token using secret key
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) {
+            // Clear the expired Cookie
+            res.clearCookie('authToken');
             // if verfication failed, return a forbiden error
             console.error('JWT verification failed', err)
             return res.status(403).json({message: 'Invalid or expired token'})
